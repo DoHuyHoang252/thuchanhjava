64e4f6c6694478b00dcd60b9f0a9a3dba8a8b2ba
Huy Hoàng <97439160+DoHuyHoang252@users.noreply.github.com>
2022-03-02 13:51:03 +0700

    second commit

--- BaiThucHanh0203/src/App90.java
+++ BaiThucHanh0203/src/App90.java
@@ -2,13 +2,21 @@ import java.util.Scanner;
 public class App90 {
     public static void main(String[] args) throws Exception {
       String chuoi;
+
       char kyTu;
-      Scanner scanner = new Scanner(System.in);  
+    
+      Scanner scanner = new Scanner(System.in);
+    
       System.out.println("Nhập vào chuỗi bất kỳ: ");
+    
       chuoi = scanner.nextLine();
-      String[] words = chuoi.split("\\s");
-      for (String w : words) {
-        System.out.println(w);
-       }
+    
+      System.out.println("Các ký tự có trong chuỗi là: ");
+    
+        for (int i = 0; i < chuoi.length(); i++){
+            kyTu = chuoi.charAt(i);
+            System.out.println(kyTu);
+    
+        }
     }
-}
+  }
